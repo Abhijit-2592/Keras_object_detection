e3fe79786a760469e42dceefaed9a41bb6dac648
Abhijit-2592 <balaabhijit@yahoo.com>
2018-01-17 10:38:28 +0530

    map rpn conv layer to 512-d featuremap

--- Faster_RCNN_API/keras_frcnn/nn_arch_inceptionv3.py
+++ Faster_RCNN_API/keras_frcnn/nn_arch_inceptionv3.py
@@ -239,11 +239,11 @@ def rpn(base_layers, num_anchors,trainable=True):
     """ RPN architecture Adopted from "Faster R-CNN: Towards Real-Time Object 
         Detection with Region Proposal Networks"
         
-        The number 768 is the output depth of the mixed-6 layer
+        512-depth feature-map
     
     """
 
-    x = Conv2D(768, (3, 3), padding='same', activation='relu', kernel_initializer='normal', name='rpn_conv1',trainable=trainable)(base_layers)
+    x = Conv2D(512, (3, 3), padding='same', activation='relu', kernel_initializer='normal', name='rpn_conv1',trainable=trainable)(base_layers)
 
     x_class = Conv2D(num_anchors, (1, 1), activation='sigmoid', kernel_initializer='uniform', name='rpn_out_class',trainable=trainable)(x)
     x_regr = Conv2D(num_anchors * 4, (1, 1), activation='linear', kernel_initializer='zero', name='rpn_out_regress',trainable=trainable)(x)
